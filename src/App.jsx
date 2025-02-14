7ab16753f219ddf1d600bf42bc9cd7c79f06e894
ray <rayem3@gmail.com>
2025-02-14 12:54:33 -0500

    add transitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import TimerControls from './TimerControls'
 import TIMER_STATES from './TimerStates'  
@@ -9,30 +9,33 @@ import Timer from './Timer';
 function App() {
   const [timerState, setTimerState] = useState(TIMER_STATES.SET)
   const [duration, setDuration] = useState(3); 
+  const [transitioning, setTransitioning] = useState(false);
+  useEffect(()=>{setTransitioning(true)},[])
 
 
   const handleSetTimerState = (newState) => setTimerState(newState) 
   const handleSetDuration = (newDuration) => setDuration(newDuration) 
  
   const buttonClass="px-4 py-2 m-1 bg-[#1a1a1a] text-white rounded-lg"
+  const transClass="   transition-opacity duration-2000 ease-in-out  "
   return (
     <>
-        <div className="card">
-      <h2>Meditate daily! Start small and build your mindfulness and headspace little by little.</h2>
-
+      <div className={"card"+ transClass + (!transitioning ? ' opacity-0 ' : ' opactiy-100 ') }>
+        {timerState === TIMER_STATES.SET && <h2>Meditate daily! Start small and build your mindfulness and headspace little by little.</h2>}
         {
            timerState !== TIMER_STATES.FINISHED &&
             <Timer duration={duration} timerState={timerState} setTimerState={setTimerState} />
         }{
           timerState === TIMER_STATES.SET &&
-          <><TimerControls 
+          <>
+          <TimerControls 
             timerState={timerState} 
             setTimerState={handleSetTimerState} 
             duration={duration}
             setDuration={handleSetDuration}
           
           ></TimerControls>
-          <button className={buttonClass + " drop-shadow-lg"} onClick={()=> setTimerState(TIMER_STATES.STARTED)}> Start meditation</button></>
+          <button className={buttonClass + " drop-shadow-lg" + "   transition duration-2000 ease-in-out  "} onClick={()=> setTimerState(TIMER_STATES.STARTED)}> Start meditation</button></>
         }
        {
           timerState === TIMER_STATES.STARTED &&
--- src/MeditationFinished.jsx
+++ src/MeditationFinished.jsx
@@ -1,10 +1,18 @@
 import TIMER_STATES from "./TimerStates";
+import { useState, useEffect } from "react";
 
 const MeditationFinished = ({setTimerState}) =>{
-  return (<> 
+  const [transitioning, setTransitioning] = useState(false);
+  useEffect(()=>{setTransitioning(true)},[])
+  const transClass="   transition-opacity duration-2000 ease-in-out  "
+  
+
+
+  return (<div className={"card"+ transClass + (!transitioning ? ' opacity-0 ' : ' opactiy-100 ') }> 
+    <h2>Meditate daily! Start small and build your mindfulness and headspace little by little.</h2>
     <p>Congratulations! You must feel great! </p>
     <p>Come back tomorrow and meditate for 5 seconds longer</p>
     <p>or  <button className='bg-[#1a1a1a]' onClick={() => setTimerState(TIMER_STATES.SET)}>meditate again</button></p>
-  </>)
+  </div>)
 }
 export default MeditationFinished;
