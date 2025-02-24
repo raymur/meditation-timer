280a97e50d9d71b631abcabf910a5adf6e3ca13d
ray <rayem3@gmail.com>
2025-02-24 11:36:48 -0500

    change bell sound

--- src/PlaySound.jsx
+++ src/PlaySound.jsx
@@ -8,20 +8,16 @@ const PlaySound = ({timerState}) => {
   const [sources, setSources] = useState([])
   const [context, setContext] = useState( new AudioContext())
 
-  useEffect(()=>{ loadDogSound()},[])
+  useEffect(()=>{ loadBowlSound()},[])
   useEffect(()=>{
     if (timerState==TIMER_STATES.PENDING_STARTED || timerState == TIMER_STATES.PENDING_FINISHED){
-        playNTimes(3)
+      playSound()
     }
   }, [timerState])
 
-
-
   const buttonClass="px-4 py-2 m-1 bg-[#1a1a1a] text-white rounded-3xl text-right"
 
-
-
-  function loadDogSound(url='bell.wav') {
+  function loadBowlSound(url='triple_bowl_hit.wav') {
     var request = new XMLHttpRequest();
     request.open('GET', url, true);
     request.responseType = 'arraybuffer';
@@ -48,7 +44,6 @@ const handleMute = (mute) => {
   }
 }
 
-
 function playSound() {
     var source = context.createBufferSource(); // creates a sound source
     sources.push(source)
@@ -56,19 +51,6 @@ function playSound() {
     if (soundOn) {source.connect(context.destination);}       // connect the source to the context's destination (the speakers)
     source.start();
 
-  }
-
-  const  playNTimes =  async (n) => {
-    let i = 0
-    playSound(); 
-    var intervalID = setInterval(() => { 
-      if (i>=n-1){ 
-        clearInterval(intervalID
-      )}else{
-        playSound(); 
-      }
-      i++
-    }, 2000)
   }
 
   return(<div       className="relative flex ">
