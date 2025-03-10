cebea2b652fa535e836a8e14aa91bfe57b1b5e46
ray <rayem3@gmail.com>
2025-03-10 11:36:54 -0400

    try to redirect www

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ var app = express();
 var port = process.env.PORT || 3000;
 if(process.env.NODE_ENV === 'production') {
   app.use((req, res, next) => {
+    const host = req.header('host').split('.');
+    let isWww = null;
+    if (host[0] === 'www')
+      isWww = host.pop(0);
+    const redirectHost = host.join('.')
     if (req.header('x-forwarded-proto') !== 'https')
-      res.redirect(`https://${req.header('host')}${req.url}`)
+      res.redirect(`https://${redirectHost}${req.url}`)
+    else if (isWww)
+      res.redirect(`https://${redirectHost}${req.url}`)
     else
       next()
   })
